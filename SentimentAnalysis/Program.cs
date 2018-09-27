4f0310f566016fa7b5f26f5e196d56728e904990
Ajmot Sk <aajmot@gmail.com>
2018-09-27 14:59:56 +0530

    Formatting some files with some readable names.

--- SentimentAnalysis/Program.cs
+++ SentimentAnalysis/Program.cs
@@ -13,15 +13,14 @@ namespace SentimentAnalysis
 {
     class Program
     {
-        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Datas", "wikipedia-detox-250-line-data.tsv");
-        static readonly string _testDataPath = Path.Combine(Environment.CurrentDirectory, "Datas", "wikipedia-detox-250-line-test.tsv");
-        static readonly string _modelpath = Path.Combine(Environment.CurrentDirectory, "Datas", "Model.zip");
+
         static void Main(string[] args)
         {
             try
             {
                 //IrisService.PredictIrisFlower();
-                Main1(args);
+                //SentimentAnalysis.PredictSentiment();
+                TaxiFarePrediction.PredictTaxiFare();
             }
             catch (Exception ex)
             {
@@ -30,71 +29,6 @@ namespace SentimentAnalysis
             }
             Console.ReadKey();
         }
-        static async Task Main1(string[] args)
-        {
-            try
-            {
-                var model = await Train();
-                Evaluate(model);
-                Predict(model);
-            }
-            catch
-            {
-                throw;
-            }
-        }
-        public static async Task<PredictionModel<SentimentData, SentimentPrediction>> Train()
-        {
-            var pipeline = new LearningPipeline();
-            pipeline.Add(new TextLoader(_dataPath).CreateFrom<SentimentData>());
-            pipeline.Add(new TextFeaturizer("Features", "SentimentText"));
-            pipeline.Add(new FastTreeBinaryClassifier() { NumLeaves = 5, NumTrees = 5, MinDocumentsInLeafs = 2 });
-            PredictionModel<SentimentData, SentimentPrediction> model =
-    pipeline.Train<SentimentData, SentimentPrediction>();
-            await model.WriteAsync(_modelpath);
-            return model;
 
-        }
-        public static void Evaluate(PredictionModel<SentimentData, SentimentPrediction> model)
-        {
-            var testData = new TextLoader(_testDataPath).CreateFrom<SentimentData>();
-            var evaluator = new BinaryClassificationEvaluator();
-            BinaryClassificationMetrics metrics = evaluator.Evaluate(model, testData);
-
-            Console.WriteLine();
-            Console.WriteLine("PredictionModel quality metrics evaluation");
-            Console.WriteLine("------------------------------------------");
-            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
-            Console.WriteLine($"Auc: {metrics.Auc:P2}");
-            Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
-        }
-        public static void Predict(PredictionModel<SentimentData, SentimentPrediction> model)
-        {
-            IEnumerable<SentimentData> sentiments = new[]
-                {
-                    new SentimentData
-                    {
-                        SentimentText = "This is the best game I've ever played"
-                    },
-                    new SentimentData
-                    {
-                        SentimentText = "He is the best, and the article should say that."
-
-                    },
-                    new SentimentData
-                    {
-                        SentimentText = "It's good to see you."
-                    },
-                    new SentimentData
-                    {
-                        SentimentText = "It is very bad what just happened to you."
-                    }
-                };
-            var _prediction = model.Predict(sentiments);
-            foreach (var x in _prediction)
-            {
-                Console.WriteLine("Pred::::1::::" + x.Sentiment);
-            }
-        }
     }
 }
---
+++ SentimentAnalysis/TaxiFarePrediction.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.ML.Runtime.Api;
+using System;
+using System.IO;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Models;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysis
+{
+    public class TaxiFarePrediction
+    {
+        static readonly string _datapath = Path.Combine(Environment.CurrentDirectory, "Datas", "taxi-fare-train.csv");
+        static readonly string _testdatapath = Path.Combine(Environment.CurrentDirectory, "Datas", "taxi-fare-test.csv");
+        static readonly string _modelpath = Path.Combine(Environment.CurrentDirectory, "Datas", "Model.zip");
+
+        static class TestTrips
+        {
+            internal static readonly TaxiTrip Trip1 = new TaxiTrip
+            {
+                VendorId = "VTS",
+                RateCode = "1",
+                PassengerCount = 1,
+                TripDistance = 10.33f,
+                PaymentType = "CSH",
+                FareAmount = 0 // predict it. actual = 29.5
+            };
+        }
+        public static async Task PredictTaxiFare()
+        {
+            PredictionModel<TaxiTrip, TaxiTripFarePrediction> model = await Train();
+            Evaluate(model);
+            TaxiTripFarePrediction prediction = model.Predict(TestTrips.Trip1);
+            Console.WriteLine("Predicted fare: {0}, actual fare: 29.5", prediction.FareAmount);
+        }
+        public static async Task<PredictionModel<TaxiTrip, TaxiTripFarePrediction>> Train()
+        {
+            var pipeline = new LearningPipeline();
+            pipeline.Add(new TextLoader(_datapath).CreateFrom<TaxiTrip>(useHeader: true, separator: ','));
+            pipeline.Add(new ColumnCopier(("FareAmount", "Label")));
+            pipeline.Add(new CategoricalOneHotVectorizer("VendorId",
+                                             "RateCode",
+                                             "PaymentType"));
+            pipeline.Add(new ColumnConcatenator("Features",
+                                    "VendorId",
+                                    "RateCode",
+                                    "PassengerCount",
+                                    "TripDistance",
+                                    "PaymentType"));
+            pipeline.Add(new FastTreeRegressor());
+
+            PredictionModel<TaxiTrip, TaxiTripFarePrediction> model = pipeline.Train<TaxiTrip, TaxiTripFarePrediction>();
+            await model.WriteAsync(_modelpath);
+            return model;
+        }
+        private static void Evaluate(PredictionModel<TaxiTrip, TaxiTripFarePrediction> model)
+        {
+            var testData = new TextLoader(_testdatapath).CreateFrom<TaxiTrip>(useHeader: true, separator: ',');
+            var evaluator = new RegressionEvaluator();
+            RegressionMetrics metrics = evaluator.Evaluate(model, testData);
+            Console.WriteLine($"Rms = {metrics.Rms}");
+        }
+    }
+    public class TaxiTrip
+    {
+        [Column("0")]
+        public string VendorId;
+
+        [Column("1")]
+        public string RateCode;
+
+        [Column("2")]
+        public float PassengerCount;
+
+        [Column("3")]
+        public float TripTime;
+
+        [Column("4")]
+        public float TripDistance;
+
+        [Column("5")]
+        public string PaymentType;
+
+        [Column("6")]
+        public float FareAmount;
+    }
+
+    public class TaxiTripFarePrediction
+    {
+        [ColumnName("Score")]
+        public float FareAmount;
+    }
+}
